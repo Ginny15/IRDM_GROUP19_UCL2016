498a2120490342096ea6da42016f5f6a3e0237f7
Ginny15 <xue.hu.15@ucl.ac.uk>
2016-04-16 13:37:31 -0700

    Update ARIMA.r

--- codes/ARIMA.r
+++ codes/ARIMA.r
@@ -7,22 +7,8 @@ library(tidyr)
 library(caret)
 library(mlbench)
 # data preprocessing
-load_history <- read.csv("Load_history.csv")
-col2cvt <- 5:28
-load_history[,col2cvt] <- lapply(load_history[,col2cvt],function(x){as.numeric(gsub(",", "", x))})
-load_history_tidy <- load_history %>%  gather(hour, load, h1:h24)
-load_history_tidy$hour <- sapply(load_history_tidy$hour, function(x) strsplit(x,"h")[[1]][2])
-load_history_tidy$hour <- as.numeric(load_history_tidy$hour)
-temperature_history <- read.csv("temperature_history.csv")
-temperature_history_tidy <- temperature_history %>%  gather(hour, temp, h1:h24)
-temperature_history_tidy$hour <- sapply(temperature_history_tidy$hour, function(x) strsplit(x,"h")[[1]][2])
-temperature_history_tidy$hour <- as.numeric(temperature_history_tidy$hour)
-temperature_history_tidy <- spread(temperature_history_tidy, key = station_id, value = temp)
-colnames(temperature_history_tidy)[5:15] <- sapply(
-  colnames(temperature_history_tidy)[5:15],function(x) paste("station",x,sep="_")
-)
-whole = left_join(load_history_tidy,temperature_history_tidy,
-                  by=c("year","month","day","hour"))
+source("data_preprocessing.r")
+ 
 for (i in 1:17){
   whole[[i]] <- as.numeric(whole[[i]])
 }
@@ -93,7 +79,7 @@ for (j in zoneID){
     test <- window(loadTS, start=1 + (i+k)/24, end=1 + (i+k+n)/24)
     xreg <- xMatrix[i:(i+k),-1]
     xregTest <- xMatrix[(1+i+k):(1+i+k+n),-1]
-    model <- Arima(training, xreg=xreg,order=c(0,0,0),seasonal = list(order=c(0,0,0)))
+    model <- Arima(training, xreg=xreg,order=c(0,0,0),seasonal = list(order=c(1,0,0)))
     fore.load <-forecast(model,xreg=xregTest)
     back.load <- fitted(model)
     # RMSEtrain[i,index.j] <- RMSE(training,back.load)
