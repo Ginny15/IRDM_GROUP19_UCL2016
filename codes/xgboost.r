b70e6edf136bf93dbb4d9adf67bac7c670de9d51
Ginny15 <xue.hu.15@ucl.ac.uk>
2016-04-16 13:57:13 -0700

    Update xgboost.r

--- codes/xgboost.r
+++ codes/xgboost.r
@@ -12,38 +12,7 @@ library(readr)
 require(gbm)
 
 # data preprocessing
-#read the original data and remove the comma inside load_history data
-setwd("E:/GoogleDrive/Academic/Information_Retrieval_and_Data_Mining/Group")
-load_history <- read.csv("Load_history.csv")
-
-#omit weeks with missing value, i.e those weeks need to be predicted
-load_history <- na.omit(load_history)
-col2cvt <- 5:28
-load_history[,col2cvt] <- lapply(load_history[,col2cvt],function(x){as.numeric(gsub(",", "", x))})
-
-#tidy load_history data into the format we need to input to the model
-#transform the row into column and make it hourly time series data
-load_history_tidy <- load_history %>%  gather(hour, load, h1:h24)
-load_history_tidy$hour <- sapply(load_history_tidy$hour, function(x) strsplit(x,"h")[[1]][2])
-load_history_tidy$hour <- as.numeric(load_history_tidy$hour)
-
-#read the original data and remove the comma inside load_history data
-temperature_history <- read.csv("temperature_history.csv")
-
-#remove the incomplete data of the last day
-temperature_history <- na.omit(temperature_history)
-
-#transform temperature data into time series and list the data of each station
-temperature_history_tidy <- temperature_history %>%  gather(hour, temp, h1:h24)
-temperature_history_tidy$hour <- sapply(temperature_history_tidy$hour, function(x) strsplit(x,"h")[[1]][2])
-temperature_history_tidy$hour <- as.numeric(temperature_history_tidy$hour)
-temperature_history_tidy <- spread(temperature_history_tidy, key = station_id, value = temp)
-colnames(temperature_history_tidy)[5:15] <- sapply(
-  colnames(temperature_history_tidy)[5:15],function(x) paste("station",x,sep="_")
-)
-
-#merge the original history load data frame with temperature data of each station
-whole = left_join(load_history_tidy,temperature_history_tidy, by=c("year","month","day","hour"))
+source("data_preprocessing.r") 
 
 for (i in 1:17){
   whole[[i]] <- as.numeric(whole[[i]])
@@ -62,7 +31,7 @@ Acf(log_value)
 Pacf(log_value)
 
 #The Augmented Dickey-Fuller(ADF) t-statistic test: small p-values suggest the data is stationary
-#and doesn’t need to be differenced stationarity
+#and does not need to be differenced stationarity
 adf.test(log_value,"stationary")
 
 #The Kwiatkowski-Phillips-Schmidt-Shin (KPSS) test; here accepting the null hypothesis means that
@@ -189,22 +158,10 @@ xgb_importance_zone18
 plot(xgb_importance_zone18,10)
 
 # backcasting and forcasting
-#1.read the data that needs to predict
-setwd("/Users/Graceyh/Desktop/IRDM/Groupwork/IRDMData")
-load_pred_all_zones <- read.csv("Backcast&Forecast.csv")
-load_pred_all_zones$year<-as.factor(load_pred_all_zones$year)
-load_pred_all_zones$month<-as.factor(load_pred_all_zones$month)
-load_pred_all_zones$day<-as.factor(load_pred_all_zones$day)
-load_pred_all_zones$hour<-as.factor(load_pred_all_zones$hour)
-load_pred_all_zones$zone_id <- as.factor(load_pred_all_zones$zone_id)
-#prepare for the prediction input
-xgb_pred_final_X <- load_pred_all_zones[,!(colnames(load_pred_all_zones) %in% c("load"))]
-xgb_pred_final_y <- load_pred_all_zones[,"load"]
 
 #make prediction based on xgboost model
 #1.read the data that needs to predict
-setwd("/Users/Graceyh/Desktop/IRDM/Groupwork/IRDMData")
-load_pred_all_zones <- read.csv("Backcast&Forecast.csv")
+load_pred_all_zones <- read.csv("../csv/testRaw.csv")
 
 #make prediction
 load_pred_all_zones$year<-as.factor(load_pred_all_zones$year)
@@ -216,6 +173,8 @@ load_pred_all_zones$zone_id <- as.factor(load_pred_all_zones$zone_id)
 #prepare for the prediction input
 xgb_pred_final_X <- load_pred_all_zones[,!(colnames(load_pred_all_zones) %in% c("load"))]
 xgb_pred_final_y <- load_pred_all_zones[,"load"]
+
+fit.xgb <- train(load~., data=load_pred_all_zones, method = "xgbTree", trControl = control)
 
 pred_final <-predict(fit.xgb, xgb_pred_final_X)
 
@@ -230,5 +189,5 @@ load_pred_all_zones_new$month <- as.numeric(load_pred_all_zones_new$month)
 load_pred_all_zones_new$day <- as.numeric(load_pred_all_zones_new$day)
 load_pred_all_zones_new_group <- load_pred_all_zones_new[order(load_pred_all_zones_new$day,load_pred_all_zones_new$month,load_pred_all_zones_new$year),]
 View(load_pred_all_zones_new_group)
-write.csv(load_pred_all_zones,"IRDMGroupPrediction.csv")
+write.csv(load_pred_all_zones,"../csv/IRDMGroupPrediction.csv")
 
